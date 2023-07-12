6d5b376f9cc223d200e974a533328f8527fabcd1
doathe <nisa.tur@gmail.com>
2023-07-13 01:40:46 +0300

    first commit

---
+++ SipayWebAPI/SipayWebAPI/Models/ResponseModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SipayWebAPI.Models
+{
+    // Response Model created.
+    public class ResponseModel<T>
+    {
+        public ResponseModel(T Data)
+        {
+            this.StatusCode = 200;
+            this.Message = "OK";
+            this.Data = Data;
+
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ SipayWebAPI/SipayWebAPI/Program.cs
@@ -0,0 +1,31 @@
+using FluentValidation.AspNetCore;
+using SipayWebAPI.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Fluent Validation added.
+builder.Services.AddControllers()
+    .AddFluentValidation(configuration =>
+    {
+        configuration.RegisterValidatorsFromAssemblyContaining<UserValidator>();
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
