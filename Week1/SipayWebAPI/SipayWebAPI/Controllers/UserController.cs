265c413d295b1ac283a607a43f0b2ff74d8a753a
doathe <nisa.tur@gmail.com>
2023-07-19 01:54:07 +0300

    chore: add week2 and week3 folders; add first project to week2 folder

--- Week1/SipayWebAPI/SipayWebAPI/Controllers/UserController.cs
+++ Week1/SipayWebAPI/SipayWebAPI/Controllers/UserController.cs
@@ -1,153 +1,149 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SipayWebAPI.Entities;
 using SipayWebAPI.Models;
-using System.Net;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace SipayWebAPI.Controllers;
 
-namespace SipayWebAPI.Controllers
+[Route("api/[controller]s")]
+[ApiController]
+public class UserController : ControllerBase
 {
-    [Route("api/[controller]s")]
-    [ApiController]
-    public class UserController : ControllerBase
+    // Defined list property for user data
+    private List<User> list;
+
+    // Dummy Data added in Constructor
+    public UserController()
     {
-        // Defined list property for user data
-        private List<User> list;
+        list = new List<User>();
 
-        // Dummy Data added in Constructor
-        public UserController()
+        list.Add(new User { Id = 1, Name = "Doğa", Lastname = "Turhan", Email = "doga.tur@gmail.com", Password = "12345aA.", Age = 22 });
+        list.Add(new User { Id = 2, Name = "Nisa", Lastname = "Turhan", Email = "nisa@gmail.com", Password = "12345aA.", Age = 20 });
+        list.Add(new User { Id = 3, Name = "Kardem", Lastname = "Mese", Email = "karr@gmail.com", Password = "12345aA.", Age = 23 });
+    }
+
+    // Action Methods added and Service operations done inside them.
+
+    // GET: api/<UserController>
+    [HttpGet]
+    public ResponseModel<IEnumerable<User>> GetAll()
+    {
+        return new ResponseModel<IEnumerable<User>>(list);                          // Returns list that I created on constructor.
+    }
+
+    // GET api/<UserController>s/{id}
+    [HttpGet("{id}")]
+    public IActionResult Get([FromRoute] int id)
+    {
+        var responseData = list.FirstOrDefault(i => i.Id == id);                    // Finds object that have entered id from route.
+        if (responseData != null)
         {
-            list = new List<User>();
-
-            list.Add(new User { Id = 1, Name = "Doğa", Lastname = "Turhan", Email = "doga.tur@gmail.com", Password = "12345aA.", Age = 22 });
-            list.Add(new User { Id = 2, Name = "Nisa", Lastname = "Turhan", Email = "nisa@gmail.com", Password = "12345aA.", Age = 20 });
-            list.Add(new User { Id = 3, Name = "Kardem", Lastname = "Mese", Email = "karr@gmail.com", Password = "12345aA.", Age = 23 });
+            return Ok(new ResponseModel<User>(responseData));                       // If the Data is not null returns Data that found.
         }
+        else
+            return NotFound();                                                      // If the Data is null returns 404 Not Found.
+    }
 
-        // Action Methods added and Service operations done inside them.
+    // POST api/<UserController>
+    [HttpPost]
+    public IActionResult Post([FromBody] User user)
+    {
+        var id = list.Count();
+        user.Id = (id + 1);                                                         // Finds the count of the list and increments it by one, adding it to the id.
+        list.Add(user);                                                             // Entered User data is added to the list.
 
-        // GET: api/<UserController>
-        [HttpGet]
-        public ResponseModel<IEnumerable<User>> GetAll()
+        return Created("User created",new ResponseModel<IEnumerable<User>>(list));  // With 201 Created, the list is returned.
+    }
+
+    // PUT api/<UserController>s/{id}
+    [HttpPut("{id}")]
+    public IActionResult Put([FromRoute] int id, [FromBody] User user)
+    {
+        var updateModel = list.FirstOrDefault(i => i.Id == id);                     // Finds object that have entered id from route.
+        if (updateModel != null)
         {
-            return new ResponseModel<IEnumerable<User>>(list);                          // Returns list that I created on constructor.
+            user.Id = id;                                                           // If data is not null, it syncs the id of the entered user value to the id given in the Route.
+            list.Remove(updateModel);                                               // It deletes the value found with the id entered in the route from the list.
+            list.Add(user);                                                         // It adds the entered user value to the list, so that the requested data is updated.
+            return Ok(new ResponseModel<User>(user));                               // Returns the updated data.
         }
+        else
+            return NotFound();                                                      // If the Data is null returns 404 Not Found.
+    }
 
-        // GET api/<UserController>s/{id}
-        [HttpGet("{id}")]
-        public IActionResult Get([FromRoute] int id)
+    // DELETE api/<UserController>s/{id}
+    [HttpDelete("{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        var responseData = list.FirstOrDefault(i => i.Id == id);                    // Finds object that have entered id from route.
+        if (responseData != null)
         {
-            var responseData = list.FirstOrDefault(i => i.Id == id);                    // Finds object that have entered id from route.
-            if (responseData != null)
+            list.Remove(responseData);                                              // If data is not null, It deletes the value found with the id entered in the route from the list.
+            return Ok(new ResponseModel<IEnumerable<User>>(list));                  // Returns list.
+        }
+        else
+            return NotFound();                                                      // If the Data is null returns 404 Not Found.
+    }
+
+    // PATCH api/<UserController>s/{id}
+    [HttpPatch("{id}")]
+    public IActionResult Patch([FromRoute] int id, string password)
+    {
+        var responseData = list.FirstOrDefault(i => i.Id == id);                    // Finds object that have entered id from route.
+        if (responseData != null)
+        {
+            list.Remove(responseData);                                              // Deletes the value found with the id entered in the route from the list.
+            responseData.Password = password;                                       // Sets the password of the found Data to the value to be changed.
+            list.Add(responseData);                                                 // It adds the entered user value to the list, so that the requested data is updated.
+            return Ok(new ResponseModel<User>(responseData));                       // Returns the updated data.
+        }
+        else
+            return NotFound();                                                      // If the Data is null returns 404 Not Found.
+    }
+
+    // GET api/<UserController>s/filter?filterValue=a
+    [HttpGet("filter")]
+    public IActionResult GetByFilter([FromQuery] string filterValue)
+    {
+        var filteredData = list.Where(user => user.Name.Contains(filterValue) || user.Lastname.Contains(filterValue)).ToList(); //Checks if there is a value with the entered filter value.
+        if (filteredData != null)
+        {
+            return Ok(new ResponseModel<IEnumerable<User>>(filteredData));          // If Data is not null, it returns filtered Data.
+        }
+        else
+            return NotFound();                                                      // If the Data is null returns 404 Not Found.
+    }
+
+    // GET api/<UserController>s/sort?sortBy=name&sortType=abc
+    [HttpGet("sort")]
+    public IActionResult GetBySort([FromQuery] string sortBy, [FromQuery] string sortType)  // abc = ascending, cba = descending
+    {
+        if (sortType == "abc")
+        {
+            if (sortBy == "name")
             {
-                return Ok(new ResponseModel<User>(responseData));                       // If the Data is not null returns Data that found.
+                var sortedData = list.OrderBy(name => name.Name);                   // Sorts ascending by name.
+                return Ok(sortedData);                                              // Returns sorted Data.
             }
-            else
-                return NotFound();                                                      // If the Data is null returns 404 Not Found.
-        }
-
-        // POST api/<UserController>
-        [HttpPost]
-        public IActionResult Post([FromBody] User user)
-        {
-            var id = list.Count();
-            user.Id = (id + 1);                                                         // Finds the count of the list and increments it by one, adding it to the id.
-            list.Add(user);                                                             // Entered User data is added to the list.
-
-            return Created("User created",new ResponseModel<IEnumerable<User>>(list));  // With 201 Created, the list is returned.
-        }
-
-        // PUT api/<UserController>s/{id}
-        [HttpPut("{id}")]
-        public IActionResult Put([FromRoute] int id, [FromBody] User user)
-        {
-            var updateModel = list.FirstOrDefault(i => i.Id == id);                     // Finds object that have entered id from route.
-            if (updateModel != null)
+            else if (sortBy == "lastname")
             {
-                user.Id = id;                                                           // If data is not null, it syncs the id of the entered user value to the id given in the Route.
-                list.Remove(updateModel);                                               // It deletes the value found with the id entered in the route from the list.
-                list.Add(user);                                                         // It adds the entered user value to the list, so that the requested data is updated.
-                return Ok(new ResponseModel<User>(user));                               // Returns the updated data.
+                var sortedData = list.OrderBy(name => name.Lastname);               // Sorts ascending by lastname.
+                return Ok(sortedData);                                              // Returns sorted Data.
             }
-            else
-                return NotFound();                                                      // If the Data is null returns 404 Not Found.
         }
-
-        // DELETE api/<UserController>s/{id}
-        [HttpDelete("{id}")]
-        public IActionResult Delete([FromRoute] int id)
+        else if (sortType == "cba")
         {
-            var responseData = list.FirstOrDefault(i => i.Id == id);                    // Finds object that have entered id from route.
-            if (responseData != null)
+            if (sortBy == "name")
             {
-                list.Remove(responseData);                                              // If data is not null, It deletes the value found with the id entered in the route from the list.
-                return Ok(new ResponseModel<IEnumerable<User>>(list));                  // Returns list.
+                var sortedData = list.OrderByDescending(name => name.Name);         // Sorts descending by name.
+                return Ok(sortedData);                                              // Returns sorted Data.
             }
-            else
-                return NotFound();                                                      // If the Data is null returns 404 Not Found.
+            else if (sortBy == "lastname")
+            {
+                var sortedData = list.OrderByDescending(name => name.Lastname);     // Sorts descending by lastname.
+                return Ok(sortedData);                                              // Returns sorted Data.
+            }
         }
 
-        // PATCH api/<UserController>s/{id}
-        [HttpPatch("{id}")]
-        public IActionResult Patch([FromRoute] int id, string password)
-        {
-            var responseData = list.FirstOrDefault(i => i.Id == id);                    // Finds object that have entered id from route.
-            if (responseData != null)
-            {
-                list.Remove(responseData);                                              // Deletes the value found with the id entered in the route from the list.
-                responseData.Password = password;                                       // Sets the password of the found Data to the value to be changed.
-                list.Add(responseData);                                                 // It adds the entered user value to the list, so that the requested data is updated.
-                return Ok(new ResponseModel<User>(responseData));                       // Returns the updated data.
-            }
-            else
-                return NotFound();                                                      // If the Data is null returns 404 Not Found.
-        }
-
-        // GET api/<UserController>s/filter?filterValue=a
-        [HttpGet("filter")]
-        public IActionResult GetByFilter([FromQuery] string filterValue)
-        {
-            var filteredData = list.Where(user => user.Name.Contains(filterValue) || user.Lastname.Contains(filterValue)).ToList(); //Checks if there is a value with the entered filter value.
-            if (filteredData != null)
-            {
-                return Ok(new ResponseModel<IEnumerable<User>>(filteredData));          // If Data is not null, it returns filtered Data.
-            }
-            else
-                return NotFound();                                                      // If the Data is null returns 404 Not Found.
-        }
-
-        // GET api/<UserController>s/sort?sortBy=name&sortType=abc
-        [HttpGet("sort")]
-        public IActionResult GetBySort([FromQuery] string sortBy, [FromQuery] string sortType)  // abc = ascending, cba = descending
-        {
-            if (sortType == "abc")
-            {
-                if (sortBy == "name")
-                {
-                    var sortedData = list.OrderBy(name => name.Name);                   // Sorts ascending by name.
-                    return Ok(sortedData);                                              // Returns sorted Data.
-                }
-                else if (sortBy == "lastname")
-                {
-                    var sortedData = list.OrderBy(name => name.Lastname);               // Sorts ascending by lastname.
-                    return Ok(sortedData);                                              // Returns sorted Data.
-                }
-            }
-            else if (sortType == "cba")
-            {
-                if (sortBy == "name")
-                {
-                    var sortedData = list.OrderByDescending(name => name.Name);         // Sorts descending by name.
-                    return Ok(sortedData);                                              // Returns sorted Data.
-                }
-                else if (sortBy == "lastname")
-                {
-                    var sortedData = list.OrderByDescending(name => name.Lastname);     // Sorts descending by lastname.
-                    return Ok(sortedData);                                              // Returns sorted Data.
-                }
-            }
-
-            return NotFound();                                                          // Returns 404 Not Found.
-        }
+        return NotFound();                                                          // Returns 404 Not Found.
     }
 }
---
+++ Week2/SipayWebAPI/SipayWebAPI/Validators/UserValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using SipayWebAPI.Entities;
+
+namespace SipayWebAPI.Validators
+{
+    // Validations added for User Model with Fluent Validation.
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            // Name Required and it's length must be between 3 and 50 characters.
+            RuleFor(user => user.Name)
+                .NotNull().NotEmpty().WithMessage("Please ensure you have entered your {PropertyName}")
+                .Length(3, 50).WithMessage("Please ensure you have entered your {PropertyName} between 3 and 50 characters.");
+
+            // Lastname Required and it's length must be between 3 and 50 characters.
+            RuleFor(user => user.Lastname)
+                .NotNull().NotEmpty().WithMessage("Please ensure you have entered your {PropertyName}")
+                .Length(3, 50).WithMessage("Please ensure you have entered your {PropertyName} between 3 and 50 characters.");
+
+            // Email Required and it must be in Email Format.
+            RuleFor(user => user.Email)
+                .NotNull().NotEmpty().WithMessage("Please ensure you have entered your {PropertyName}")
+                .EmailAddress().WithMessage("Please ensure you have entered your {PropertyName} in the correct format.");
+
+            // Password Required and it's length must be greater than 7. It must contain at least one uppercase letter and one digit.
+            RuleFor(user => user.Password)
+                .NotNull().NotEmpty().WithMessage("Please ensure you have entered your {PropertyName}")
+                .MinimumLength(7).WithMessage("Please ensure you have entered your {PropertyName} more than 7 characters.")
+                .Matches(@"^(?=.*[A-Z])(?=.*\d).+$").WithMessage("Please ensure you have entered your {PropertyName} at least one uppercase letter and one digit.");
+
+            // Age Required and it must greater than or equal to 18.
+            RuleFor(user => user.Age)
+                .NotNull().NotEmpty().WithMessage("Please ensure you have entered your {PropertyName}")
+                .GreaterThanOrEqualTo(18).WithMessage("Please ensure you have entered your {PropertyName} greater than or equal to 18+.");
+        }
+    }
+}
